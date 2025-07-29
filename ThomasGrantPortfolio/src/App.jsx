b9e60cedce594b614809a1bb4826b2563d531146
TomRgrant <111927422+TomRgrant@users.noreply.github.com>
2025-07-29 10:45:03 +0100

    added projects container and css, also added dummy assest for project imgs to assests folder

--- ThomasGrantPortfolio/src/App.jsx
+++ ThomasGrantPortfolio/src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import LandingPage from './Containers/LandingPage'
+import Projects from './Containers/Projects'
 
 function App() {
 
   return (
     <>
       <LandingPage />
+      <Projects />
     </>
   )
 }
---
+++ ThomasGrantPortfolio/src/Containers/Projects.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "../Styles/Projects.css";
+
+
+const projects = () => {
+  
+
+
+
+
+
+    return(
+
+        <>
+        <div className="projects-wrapper">
+            <h1>Projects</h1>
+        </div>
+        
+        </>
+
+    )
+}
+
+export default projects;
