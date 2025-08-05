0c93c11c4388a9b6c0195a5d67fdb9c69f6682aa
TomRgrant <111927422+TomRgrant@users.noreply.github.com>
2025-08-05 11:58:38 +0100

    Added fake window frame css to project imgs

--- ThomasGrantPortfolio/src/Components/ProjectCards.jsx
+++ ThomasGrantPortfolio/src/Components/ProjectCards.jsx
@@ -1,52 +1,58 @@
 import React from "react";
-
 import "../Styles/Showcase.css";
 
 const ProjectCards = ({ projects }) => {
-    const projectNodes = projects.map((project, index) => {
-
-        
-        const tagsNodes = project.tags?.map((tag, tagIndex) => (
-            <div className="tag" key={tagIndex}>
-                <p className="tag">{tag}</p>
-            </div>
-        ));
-
-        return (
-            <div className="card-wrapper" key={index}>
-                <div className="left">
-                    <h1><span>{project.title}</span></h1>
-                    <h3>{project.type}</h3>
-
-                    <div className="tags">
-                        {tagsNodes}
-                    </div>
-
-                    <p>{project.description}</p>
-                    <p>{project.desc2}</p>
-
-                    <div className="project-buttons">
-                        <button>View More</button>
-                    </div>
-                </div>
-
-                <div className="right">
-                    <div className="fake-window">
-                        <div className="red" />
-                        <div className="green" />
-                        <div className="yellow" />
-                    </div>
-                    <img className="project-img" src={project.img} alt={`${project.title} screenshot`} />
-                </div>
-            </div>
-        );
-    });
+  const projectNodes = projects.map((project, index) => {
+    const tagsNodes = project.tags?.map((tag, tagIndex) => (
+      <div className="tag" key={tagIndex}>
+        <p className="tag">{tag}</p>
+      </div>
+    ));
 
     return (
-        <>
-            {projectNodes}
-        </>
+      <div className="card-wrapper" key={index}>
+        <div className="left">
+          <h1><span>{project.title}</span></h1>
+          <h3>{project.type}</h3>
+
+          <div className="tags">
+            {tagsNodes}
+          </div>
+
+          <p>{project.description}</p>
+          <p>{project.desc2}</p>
+
+          <div className="project-buttons">
+            <button>View More</button>
+          </div>
+        </div>
+
+        <div className="right">
+  <div className="screen-frame">
+    <div className="fake-browser">
+      <div className="fake-window">
+        <div className="traffic-lights">
+          <div className="red" />
+          <div className="yellow" />
+          <div className="green" />
+        </div>
+        <div className="title">{project.title}</div>
+      </div>
+      <img
+        className="project-img"
+        src={project.img}
+        alt={`${project.title} screenshot`}
+      />
+    </div>
+  </div>
+</div>
+
+
+      </div>
     );
+  });
+
+  return <>{projectNodes}</>;
 };
 
 export default ProjectCards;
